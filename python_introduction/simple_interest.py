f4d3c0163818745ef6afb0a7e10dbc52b1f5c243
Ponagalo <seroganyanemathaba@gmail.com>
2025-09-13 15:08:27 +0200

    I have added a new file called simple_interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,6 @@
+#Declaring and initializing my variables
+principle = 1000
+rate = 0.05
+time = 3
+simple_interest = (principle * rate * time) #Calculating the simple interest
+print("The simple interest rate is:", simple_interest )
