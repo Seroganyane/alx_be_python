ec2af789cb81908b8604cfd7acbaa94aaa4a6eef
Ponagalo <seroganyanemathaba@gmail.com>
2025-09-13 15:44:13 +0200

    Added a  file that gets usrs future age

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,14 @@
+# This program calculates your age in a future year
+# Get the current age from the user
+age = int(input("Enter your current age: "))
+
+# Get the future year from the user (assuming the current year is 2023)
+print("in 2050,you will be ",age +27,"years old")
+
+#Get the future year from the user using current year
+#get useryear using input user age
+useryear = 2025 - age
+#Calculate the age in the future year
+futurage = 2050 - useryear
+#Display the result
+print("In 2050, you will be", futurage, "years old.")
