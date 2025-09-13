d649d7f4ad8cffd06ac89f168d0bcc8b877587e4
Ponagalo <seroganyanemathaba@gmail.com>
2025-09-13 14:46:49 +0200

    Initial python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+# Addition
+sum_result = number1 + number2
+# Subtraction
+difference = number2 - number1
+# Multiplication
+product = number1 * number2
+print("Addition of number1 and number2 is:", sum_result)
+print("Subtraction of number1 and number2 is:", difference)
+print("Multiplication of number1 and number2 is:", product)
