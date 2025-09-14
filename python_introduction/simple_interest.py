d45d169aee21c7d3712b258a0d434ed54a4129da
Ponagalo <seroganyanemathaba@gmail.com>
2025-09-14 13:46:54 +0200

    Fixed declaration of my variables and the simple interest calculation

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
 #Declaring and initializing my variables
-principle = 1000
+principal = 1000
 rate = 0.05
 time = 3
-interest = (principle * rate * time) #Calculating the simple interest
-print("The simple interest is:", interest )
+interest = (principal * rate * time) #Calculating the simple interest
+print("The simple interest  is:", interest )
