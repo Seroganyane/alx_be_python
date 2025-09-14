2b150352ed9d63641750713d1e6127c0838d0d58
Ponagalo <seroganyanemathaba@gmail.com>
2025-09-14 13:42:12 +0200

    Fixed and error in the file for calculating simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,5 +2,5 @@
 principle = 1000
 rate = 0.05
 time = 3
-simple_interest = (principle * rate * time) #Calculating the simple interest
-print("The simple interest rate is:", simple_interest )
+interest = (principle * rate * time) #Calculating the simple interest
+print("The simple interest is:", interest )
