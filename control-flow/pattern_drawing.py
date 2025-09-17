a154b69c0041c4d59699c6cbd106ecd41029c2b3
Ponagalo <seroganyanemathaba@gmail.com>
2025-09-17 10:08:47 +0200

    Drawing a square according to input size

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,10 @@
+size = int(input("Enter the size of the pattern: "))
+j=0
+while j<size:
+    for i in range(size):
+        print("*", end=" ")
+        
+        
+    print()
+    j += 1
+      
