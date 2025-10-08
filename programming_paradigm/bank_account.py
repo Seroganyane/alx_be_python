01f658cacaea7c0e6b6bf08ec7a64fe6feb2b490
Ponagalo <seroganyanemathaba@gmail.com>
2025-10-08 23:59:43 +0200

    OOP

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __init__(self, account_balance=0):
+        self.account_balance = account_balance
+    def deposit(self, amount):
+        self.account_balance += amount
+        return self.account_balance
+    def withdraw(self, amount):
+        if self.account_balance - amount >= 0:
+            self.account_balance -= amount
+            return True
+        else:
+         return False
+    def display_balance(self):
+        print(f"Your balance is ${self.account_balance:.2f}.")
+    
