8ab50cb2f8c4a685ac1f44571fa50045d474fe01
Ponagalo <seroganyanemathaba@gmail.com>
2025-10-12 20:15:27 +0200

    OOP in action

---
+++ OOP/book_class.py
@@ -0,0 +1,28 @@
+class Book:
+    def __init__(self, title: str, author: str, year: int):
+        """
+        Initializes a Book instance with title, author, and year.
+        """
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        """
+        Prints a message when the Book instance is deleted.
+        """
+        print(f"Deleting '{self.title}'")
+
+    def __str__(self):
+        """
+        Returns a string representation of the Book instance.
+        Format: "(title) by (author), published in (year)".
+        """
+        return f"'{self.title}' by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        """
+        Returns an official string representation of the Book instance.
+        Format: f"Book('{self.title}', '{self.author}', {self.year})".
+        """
+        return f"Book('{self.title}', '{self.author}', {self.year})"
